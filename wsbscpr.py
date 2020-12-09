8e8ee2867bfd5cbdd3e2f620e96389d042f7f4be
Automage <pranav92000@gmail.com>
2020-12-10 00:15:12 +0400

    Added intro msg and main()

--- wsbscpr.py
+++ wsbscpr.py
@@ -3,8 +3,19 @@ import os
 import argparse
 import json
 import praw
+from colorama import init, Fore
 from dotenv import load_dotenv
 
+# Doom font
+INTRO_MSG = '''
+ _    _ ___________  _____ _____ ______  ___  ______ ___________ 
+| |  | /  ___| ___ \/  ___/  __ \| ___ \/ _ \ | ___ \  ___| ___ \\
+| |  | \ `--.| |_/ /\ `--.| /  \/| |_/ / /_\ \| |_/ / |__ | |_/ /
+| |/\| |`--. \ ___ \ `--. \ |    |    /|  _  ||  __/|  __||    / 
+\  /\  /\__/ / |_/ //\__/ / \__/\| |\ \| | | || |   | |___| |\ \ 
+ \/  \/\____/\____/ \____/ \____/\_| \_\_| |_/\_|   \____/\_| \_|
+                                                                                                                       
+'''
 
 def setup_args():
     parser = argparse.ArgumentParser()
@@ -27,47 +38,54 @@ def ticker_search(comment, tickers):
     print('[', comment.permalink, ']')
     print()
 
-# Env Setup
+def main():
+    print(Fore.RED + INTRO_MSG)
 
-SECRET, ID, USER_AGENT = setup_env()
-parser = setup_args()
-# print("SECRET=%s ID=%s USER_AGENT=%s" % (SECRET, ID, USER_AGENT))
-# print()
+    # Env Setup
+    init(autoreset=True) # Init colorama
+    SECRET, ID, USER_AGENT = setup_env()
+    parser = setup_args()
+    # print("SECRET=%s ID=%s USER_AGENT=%s" % (SECRET, ID, USER_AGENT))
+    # print()
 
-# PRAW Setup
+    # PRAW Setup
 
-reddit = praw.Reddit(client_id=ID, client_secret=SECRET, user_agent=USER_AGENT)
+    reddit = praw.Reddit(client_id=ID, client_secret=SECRET, user_agent=USER_AGENT)
 
-# Parse arguments
+    # Parse arguments
 
-options = parser.parse_args(sys.argv[1:])
-num_posts = options.numposts
-tickers = options.tickers
+    options = parser.parse_args(sys.argv[1:])
+    num_posts = options.numposts
+    tickers = options.tickers
 
-print()
-print("Tickers:\t", tickers)
-print("#Posts: \t", num_posts)
-print()
-
-# Search posts
-
-posts = reddit.subreddit('wallstreetbets').hot(limit=(num_posts + 1))
-posts = [post for post in posts]
-
-# Skip daily discussion
-posts = posts[1:]
-
-for post in posts:
-    print(post.title)
-    print('[', post.url, ']')
     print()
-    submission = reddit.submission(id=post.id)
-    submission.comment_sort = 'top'
-    submission.comments.replace_more(limit=0)
-    i = 0
-    for comment in submission.comments.list():
-        ticker_search(comment, tickers)
-        # Limit search to top 30 comments per post
-        i = i + 1
-        if i > 30:
-            break
+    print("Tickers:\t", tickers)
+    print("#Posts: \t", num_posts)
+    print()
+
+    # Search posts
+
+    posts = reddit.subreddit('wallstreetbets').hot(limit=(num_posts + 1))
+    posts = [post for post in posts]
+
+    # Skip daily discussion
+    posts = posts[1:]
+
+    for post in posts:
+        print(post.title)
+        print('[', post.url, ']')
+        print()
+        submission = reddit.submission(id=post.id)
+        submission.comment_sort = 'top'
+        submission.comments.replace_more(limit=0)
+        i = 0
+        for comment in submission.comments.list():
+            ticker_search(comment, tickers)
+            # Limit search to top 30 comments per post
+            i = i + 1
+            if i > 30:
+                break
+
+
+if __name__ == "__main__":
+    main()
