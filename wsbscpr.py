746e8ec988bd5309a38f475337da13a6448c68fa
Automage <pranav92000@gmail.com>
2020-12-10 00:02:08 +0400

    PRAW working

--- wsbscpr.py
+++ wsbscpr.py
@@ -21,11 +21,18 @@ def setup_env():
     return os.getenv('SECRET'), os.getenv('ID'), os.getenv('USER_AGENT')
 
 
+def ticker_search(comment, tickers):
+    print('Tickers: ', tickers)
+    print(comment.body)
+    print('[', comment.permalink, ']')
+    print()
+
 # Env Setup
 
 SECRET, ID, USER_AGENT = setup_env()
 parser = setup_args()
-print("SECRET=%s ID=%s USER_AGENT=%s" % (SECRET, ID, USER_AGENT))
+# print("SECRET=%s ID=%s USER_AGENT=%s" % (SECRET, ID, USER_AGENT))
+# print()
 
 # PRAW Setup
 
@@ -36,3 +43,31 @@ reddit = praw.Reddit(client_id=ID, client_secret=SECRET, user_agent=USER_AGENT)
 options = parser.parse_args(sys.argv[1:])
 num_posts = options.numposts
 tickers = options.tickers
+
+print()
+print("Tickers:\t", tickers)
+print("#Posts: \t", num_posts)
+print()
+
+# Search posts
+
+posts = reddit.subreddit('wallstreetbets').hot(limit=(num_posts + 1))
+posts = [post for post in posts]
+
+# Skip daily discussion
+posts = posts[1:]
+
+for post in posts:
+    print(post.title)
+    print('[', post.url, ']')
+    print()
+    submission = reddit.submission(id=post.id)
+    submission.comment_sort = 'top'
+    submission.comments.replace_more(limit=0)
+    i = 0
+    for comment in submission.comments.list():
+        ticker_search(comment, tickers)
+        # Limit search to top 30 comments per post
+        i = i + 1
+        if i > 30:
+            break
